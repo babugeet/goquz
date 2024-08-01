c1050683db7c29e1e716edef02ac43948798d976
babugeet <97796199+babugeet@users.noreply.github.com>
2024-08-01 16:03:42 +0000

    code changes golang

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # goquz
 
+
+* This is a simple quiz tool which will ask user to answer.
+* Checks answer and give a final correct and incorrect answer score
+* The quiz is given as a file
+
+
+Tried to cover
+* SRP
+* packages
+
+
 * [ ] Read file from CSV
 
 * [ ] Setup base code format
--- goquz/main.go
+++ goquz/main.go
@@ -1,60 +1,13 @@
 package main
 
 import (
-	"encoding/csv"
-	"fmt"
-	"os"
-
-	"github.com/babugeet/goquz/internal/constants"
-	"github.com/babugeet/goquz/internal/variables"
+	"github.com/babugeet/goquz/goquz/internal/constants"
+	"github.com/babugeet/goquz/goquz/internal/quizutils"
 )
 
 func main() {
 
-	csvContent, _ := readCSVfile(constants.Filename)
-	for _, line := range csvContent {
-		fmt.Println(line[0])
-		checkAnswer(getUserInput(), line[1])
+	csvContent, _ := quizutils.ReadCSVfile(constants.Filename)
+	quizutils.QuizQA(csvContent)
 
-	}
-	fmt.Printf("Correct Answer %v , Wrong Answer %v", len(csvContent)-variables.failureCount, variables.failureCount)
-}
-
-func getUserInput() string {
-	var input string
-	fmt.Scan(&input)
-	return input
-}
-
-func checkAnswer(userinput, actualAnswer string) {
-	if userinput == actualAnswer {
-		// fmt.Println("Correct Answer")
-	} else {
-		// fmt.Println("Wrong Answer")
-		countFailure()
-	}
-
-}
-
-func readCSVfile(filename string) ([][]string, error) {
-
-	fmt.Println("CSV file name available is ", filename)
-	fileBytes, err := os.Open(filename)
-	if err != nil {
-		fmt.Println("Recieved Error, ", err.Error())
-		return nil, err
-	}
-	csvReader := csv.NewReader(fileBytes)
-	csvContent, err := csvReader.ReadAll()
-	if err != nil {
-		fmt.Println(err.Error())
-		return nil, err
-	}
-	fmt.Println(csvContent)
-	return csvContent, nil
-
-}
-
-func countFailure() {
-	variables.FailureCount = variables.FailureCount + 1
 }
